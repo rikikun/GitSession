6ad63d4c721e389014f65bada87225b49679936b
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2016-06-30 13:32:18 +0700

    add class

--- GitSession/src/app/app.component.ts
+++ GitSession/src/app/app.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppbComponent {
   title = 'app works!';
   num : number;
   name : string;
   score[] = [
     {num : 0,name:'love'},
     {num : 1,nammme:'15'},
-    {num : 2,name:'30'},
+    {num : 2,nkkame:'30'},
     {num : 3,name:'40'},
     {num : 4,name:'game'}
   ];
