f53408e94a2f2b2bc65411203ddaf70c987f96c1
Jutamas Puttisomboon <jutamas.put@ascendcorp.com>
2016-06-30 14:31:19 +0700

    pp

--- GitSession/src/app/app.component.ts
+++ GitSession/src/app/app.component.ts
@@ -6,15 +6,32 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppbComponent {
+export class AppComponent {
   title = 'app works!';
   num : number;
   name : string;
+  num2 : number;
+
+  c1 : string;
+
   score = [
-    {num : 0,name:'love'},
-    {num : 1,nammme:'15'},
-    {num : 2,nkkame:'30'},
-    {num : 3,name:'40'},
-    {num : 4,name:'game'}
+  {num : 0,name:'love'},
+  {num : 1,name:'15'},
+  {num : 2,name:'30'},
+  {num : 3,name:'40'},
+  {num : 4,name:'game'}
   ];
+
+  score1 = [
+  {num : 0,name:'love'},
+  {num : 1,name:'15'},
+  {num : 2,name:'30'},
+  {num : 3,name:'40'},
+  {num : 4,name:'game'}
+  ];
+
+  aaa(numbbb){
+    console.log(this.num+this.num2);
+    this.c1=this.score[this.num].name;
+  }
 }
