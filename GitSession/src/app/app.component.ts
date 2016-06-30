e8f56da52bdc0fa06b9ac5fbfda81b212df0cd19
Benyapa Chayakornwatchara <benyapa.cha@ascendcorp.com>
2016-06-30 14:41:11 +0700

    beam commit

--- GitSession/src/app/app.component.ts
+++ GitSession/src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  score = [
+    {id: 0, name: "love" },
+    {id: 1, name: "15" },
+    {id: 2, name: "30" },
+    {id: 3, name: "40" },
+    {id: 0, name: "game" }
+  ]
 }
